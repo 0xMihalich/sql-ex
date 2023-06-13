dc989f83f0ef7a5d6a7bb354491b768cd34149ca
0xMihalich <bayan-mobile@list.ru>
2023-06-13 14:00:14 +1000

    Create 039.sql

---
+++ 039.sql
@@ -0,0 +1,17 @@
+/*
+Найдите корабли, `сохранившиеся для будущих сражений`; т.е. выведенные из строя в одной битве (damaged), они участвовали в другой, произошедшей позже.
+*/
+WITH b AS (SELECT ship,
+                  name,
+                  date,
+                  result
+             FROM outcomes o
+             LEFT JOIN battles b
+               ON o.battle = b.name)
+SELECT DISTINCT
+       ship
+  FROM b
+ WHERE ship IN (SELECT r.ship
+                  FROM b r
+                 WHERE r.date < b.date
+                   AND r.result = 'damaged')
