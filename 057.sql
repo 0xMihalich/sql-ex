e28e6851fc3e57fd6c699319ff87e1201cbbfd8f
0xMihalich <bayan-mobile@list.ru>
2023-06-14 14:18:03 +1000

    Update 057.sql

--- 057.sql
+++ 057.sql
@@ -9,29 +9,29 @@ SELECT class cls,
             END) sunken
   FROM (select c.class,
                name
-		      from Classes c
+	  from Classes c
           join Ships s
-		        on c.class = s.class
+	    on c.class = s.class
          union
         select class,
-		           ship
-		      from Classes 
+               ship
+          from Classes 
           join Outcomes
-		        on class = ship) shp
+            on class = ship) shp
   LEFT JOIN Outcomes o
     ON shp.name = o.ship
   GROUP BY class
  HAVING SUM(CASE WHEN result = 'sunk'
                  THEN 1
-				         ELSE 0
-			       END) > 0
+		 ELSE 0
+	     END) > 0
     AND (select count(sl.name)
-	         from (select s.name,
-		                    s.class
-				           from Ships s
+	   from (select s.name,
+		        s.class
+		   from Ships s
                   union
                  select o.ship,
-				                o.ship
-				           from Outcomes o) sl
+		        o.ship
+	           from Outcomes o) sl
                   where sl.class = shp.class
                   group by sl.class) >= 3
