60cfb907160b2bfbc0c4818fe9fe820e78a791b6
0xMihalich <bayan-mobile@list.ru>
2023-06-19 17:27:10 +1000

    Update 091.sql

--- 091.sql
+++ 091.sql
@@ -2,8 +2,8 @@
 Задание: 91 (Serge I: 2015-03-20)
 C точностью до двух десятичных знаков определить среднее количество краски на квадрате.
 */
-SELECT CONVERT(NUMERIC(6, 2), AVG(VOL * 1.0)) avg_paint
-  FROM (select coalesce(sum(B_VOL),0) VOL
+SELECT CONVERT(NUMERIC(6, 2), AVG(paint * 1.0)) avg_paint
+  FROM (select coalesce(sum(B_VOL),0) paint
           from utQ
           left join utB
             on utB.B_Q_ID = utQ.Q_ID
