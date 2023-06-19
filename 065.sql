21fca0d0638f0676d390856ee90d4483bf64d433
0xMihalich <bayan-mobile@list.ru>
2023-06-19 12:24:46 +1000

    Update 065.sql

--- 065.sql
+++ 065.sql
@@ -14,15 +14,15 @@ SELECT row_number() OVER(order by maker) num,
        type
   FROM (select row_number() over(partition by maker order by maker, ord) maker_num,
                maker,
-			   type
+               type
           from (SELECT DISTINCT maker,
-		               type,
-					   CASE WHEN type = 'PC'
-					        THEN 1
-                            WHEN type = 'Laptop'
-					        THEN 2
-					        WHEN type = 'Printer'
-					        THEN 3
-                        END ord
-                 FROM product) mto
+                                type,
+                                CASE WHEN type = 'PC'
+                                     THEN 1
+                                     WHEN type = 'Laptop'
+                                     THEN 2
+                                     WHEN type = 'Printer'
+                                     THEN 3
+                                 END ord
+                  FROM product) mto
                ) mtn
